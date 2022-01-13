17555a792efcfabb3b9b99235d01f842b1c3a137
Ethan McElvaney <ewmcelvaneydesign@gmail.com>
2022-01-12 21:00:43 -0500

    Adds favicon and fixes logo problem

--- codeforthecarolinas/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ codeforthecarolinas/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,77 +13,77 @@ namespace codeforthecarolinas.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/ewmcelvaney/Desktop/Code for the Carolinas/codeforthecarolinas/codeforthecarolinas/codeforthecarolinas/_Imports.razor"
+#line 1 "/Users/ewmcelvaney/Documents/GitHub/cforthec/codeforthecarolinas/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/ewmcelvaney/Desktop/Code for the Carolinas/codeforthecarolinas/codeforthecarolinas/codeforthecarolinas/_Imports.razor"
+#line 2 "/Users/ewmcelvaney/Documents/GitHub/cforthec/codeforthecarolinas/_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/ewmcelvaney/Desktop/Code for the Carolinas/codeforthecarolinas/codeforthecarolinas/codeforthecarolinas/_Imports.razor"
+#line 3 "/Users/ewmcelvaney/Documents/GitHub/cforthec/codeforthecarolinas/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/ewmcelvaney/Desktop/Code for the Carolinas/codeforthecarolinas/codeforthecarolinas/codeforthecarolinas/_Imports.razor"
+#line 4 "/Users/ewmcelvaney/Documents/GitHub/cforthec/codeforthecarolinas/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/ewmcelvaney/Desktop/Code for the Carolinas/codeforthecarolinas/codeforthecarolinas/codeforthecarolinas/_Imports.razor"
+#line 5 "/Users/ewmcelvaney/Documents/GitHub/cforthec/codeforthecarolinas/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/ewmcelvaney/Desktop/Code for the Carolinas/codeforthecarolinas/codeforthecarolinas/codeforthecarolinas/_Imports.razor"
+#line 6 "/Users/ewmcelvaney/Documents/GitHub/cforthec/codeforthecarolinas/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/ewmcelvaney/Desktop/Code for the Carolinas/codeforthecarolinas/codeforthecarolinas/codeforthecarolinas/_Imports.razor"
+#line 7 "/Users/ewmcelvaney/Documents/GitHub/cforthec/codeforthecarolinas/_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/ewmcelvaney/Desktop/Code for the Carolinas/codeforthecarolinas/codeforthecarolinas/codeforthecarolinas/_Imports.razor"
+#line 8 "/Users/ewmcelvaney/Documents/GitHub/cforthec/codeforthecarolinas/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/ewmcelvaney/Desktop/Code for the Carolinas/codeforthecarolinas/codeforthecarolinas/codeforthecarolinas/_Imports.razor"
+#line 9 "/Users/ewmcelvaney/Documents/GitHub/cforthec/codeforthecarolinas/_Imports.razor"
 using codeforthecarolinas;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/ewmcelvaney/Desktop/Code for the Carolinas/codeforthecarolinas/codeforthecarolinas/codeforthecarolinas/_Imports.razor"
+#line 10 "/Users/ewmcelvaney/Documents/GitHub/cforthec/codeforthecarolinas/_Imports.razor"
 using codeforthecarolinas.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "/Users/ewmcelvaney/Desktop/Code for the Carolinas/codeforthecarolinas/codeforthecarolinas/codeforthecarolinas/_Imports.razor"
+#line 11 "/Users/ewmcelvaney/Documents/GitHub/cforthec/codeforthecarolinas/_Imports.razor"
 using MudBlazor;
 
 #line default
